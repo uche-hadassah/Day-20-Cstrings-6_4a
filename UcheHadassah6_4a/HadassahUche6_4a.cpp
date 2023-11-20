1965b31ebb51ae9a38f8c04db3a14da29f2860f2
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 19:56:12 +0200

    Add project files.

---
+++ UcheHadassah6_4a/HadassahUche6_4a.cpp
@@ -0,0 +1,6 @@
+/*Encryption: a) Write an encryption program which reads a string (containing only 
+alphabetic characters and spaces) and encrypts it using the following coding 
+technique: replace an ‘a’ with a ‘b’, ‘b’ with ‘c’ and so on, up to ‘z’ which is replaced 
+by an ‘a’. The space ‘ ‘ is replaced by a star ‘*’.
+Enter a word: cat
+The word encrypted is: dbu*/
