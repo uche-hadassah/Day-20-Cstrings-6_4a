13ded5dd69d8c1196da4e8b40bf8ebeb4cbfe069
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 20:10:39 +0200

    Ive pretty much written the whole code. For some reason, I'm unable to compile it

--- UcheHadassah6_4a/HadassahUche6_4a.cpp
+++ UcheHadassah6_4a/HadassahUche6_4a.cpp
@@ -3,4 +3,4 @@ alphabetic characters and spaces) and encrypts it using the following coding
 technique: replace an ‘a’ with a ‘b’, ‘b’ with ‘c’ and so on, up to ‘z’ which is replaced 
 by an ‘a’. The space ‘ ‘ is replaced by a star ‘*’.
 Enter a word: cat
-The word encrypted is: dbu*/
+The word encrypted is: dbu*/#include<iostream>#include<cstring>using namespace std;const int MAX = 100;int main(){	char word[MAX];	int i;	cout << "Enter the word you wish to encrypt:";	cin.getline(word,MAX);	for (i = 0; word[i] != '\0'; i++)//changes the alphabet	{		if (word[i] == ' ')		{			word[i] = '*';		}		else		{			word[i]++;		}	}	cout << "The word encrypted is:";	for (int j = 0; j < i; j++)	{		cout << word[j];	}	cout << endl;}
