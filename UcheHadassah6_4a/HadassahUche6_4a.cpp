84614a76f0e6ff406f6ad9e586d83c58f2447195
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 20:30:09 +0200

    Cleaned up the code and added comments

--- UcheHadassah6_4a/HadassahUche6_4a.cpp
+++ UcheHadassah6_4a/HadassahUche6_4a.cpp
@@ -1,5 +1,35 @@
-/*
-Encryption: a) Write an encryption program which reads a string 
-(containing only alphabetic characters and spaces) and encrypts it using the following coding technique: 
-replace an ‘a’ with a ‘b’, ‘b’ with ‘c’ and so on, up to ‘z’ which is replaced 
-by an ‘a’. The space ‘ ‘ is replaced by a star ‘*’.*/#include<iostream>#include<cstring>using namespace std;const int MAX = 100;int main(){	char word[MAX];	int i;	cout << "Enter the word you wish to encrypt:";	cin.getline(word,MAX);	for (i = 0; word[i] != '\0'; i++)//changes the alphabet	{		if (word[i] == ' ')		{			word[i] = '*';		}		else		{			word[i]++;			if (word[i] > 'z')			{				word[i] = 'a';			}		}	}	cout << "The word encrypted is:";	for (int j = 0; j < i; j++)	{		cout << word[j];	}	cout << endl;}
+/*Name:Uche Hadassah
+This program reads in a string and encrypts it. i.e a becomes b, b becomes c and so on
+*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int MAX = 100;
+int main()
+{
+	char word[MAX];
+	int i;
+	cout << "Enter the word you wish to encrypt:";
+	cin.getline(word,MAX);
+	for (i = 0; word[i] != '\0'; i++)//Loops through the sentence or word
+	{
+		if (word[i] == ' ')
+		{
+			word[i] = '*';
+		}
+		else
+		{
+			word[i]++;
+			if (word[i] > 'z')
+			{
+				word[i] = 'a';//Goes back to a if its > z
+			}
+		}
+	}
+	cout << "The word encrypted is:";
+	for (int j = 0; j < i; j++)
+	{
+		cout << word[j];//Outputs the encrypted word or sentence
+	}
+	cout << endl;
+}
